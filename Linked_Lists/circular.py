2bd7061ccb3ca6caf62e4bb7c0e01253859254b8
sarthak625 <sarthakn18@gmail.com>
2019-06-06 23:28:42 +0530

    Implemented circular linked list

--- Linked_Lists/circular.py
+++ Linked_Lists/circular.py
@@ -1,4 +1,4 @@
-# Implementation of single and doubly linked list in this file
+# Implementation of circular linked list in this file
 
 # The basic Node class
 class Node:
@@ -12,6 +12,8 @@ class LinkedList:
     # Initialize the linked list with a head node
     def __init__(self, data):
         self.head = Node(data)
+        self.head.next = self.head
+        self.head.prev = self.head
     
     # Add elements to the end of the linked list
     def add(self,value):
@@ -20,22 +22,29 @@ class LinkedList:
         # If the head is None, make the new node the head node
         if head == None:
             head = node
+            node.next = node
+            node.prev = node
         # If the linked list has just one node, add the node to the head's next
-        elif head.next == None:
+        elif head.next == head:
             head.next = node
+            head.prev = node
             node.prev = head
+            node.next = head
         # Else iterate till the end of the list and add the node
         else:
             current = head
-            while current.next != None:
+            while current.next != head:
                 current = current.next
             current.next = node
+            node.next = head
             node.prev = current
+            head.prev = node
     
     # Add an element to the top of the list
     def addHead(self,value):
         node = Node(value)
         node.next = self.head
+        node.prev = self.head.prev
         self.head.prev = node
         self.head = node
 
@@ -43,52 +52,57 @@ class LinkedList:
     def addAfter(self,value,newValue):
         node = Node(newValue)
         head = self.head
+        changed = False
         # Iterate till you reach the node
-        while head != None:
+        while head.next != self.head:
             if (head.data == value):
-                # If its the last node, add as it is
-                if head.next == None:
-                    head.next = node
-                    node.prev = head
-                    break
-                # Else add prev to the node after the searched node
-                else:
-                    head.next.prev = node
-                    node.next = head.next
-                    head.next = node
-                    node.prev = head
-                    break
+                head.next.prev = node
+                node.next = head.next
+                head.next = node
+                node.prev = head
+                changed=True
+                break      
             head = head.next
+        
+        if (head.data == value and not changed):
+            head.next = node
+            node.prev = head
+            node.next = self.head
+            self.head.prev = node
     
     #Add value before a certain node
     def addBefore(self,value,newValue):
         node = Node(newValue)
         head = self.head
+        changed = False
         # Iterate till you reach the node
-        while head != None:
-            if (head.data == value):
-                # If its the head node, make this node the head node
-                if head == self.head:
-                    # this
-                    self.head.prev = node
-                    node.next = self.head
-                    self.head = node
-                    # or this
-                    # addHead(self, newValue)
-                    break
-                # Else add value to the node before the searched node
-                else:
-                    head.prev.next = node
-                    node.prev = head.prev
-                    node.next = head
-                    head.prev = node
-                    break
+        while head.next != self.head:
+            if (head.data == value and head != self.head):   
+                head.prev.next = node
+                node.prev = head.prev
+                node.next = head
+                head.prev = node
+                changed = True
+                break
+            if (head.data == value and head == self.head):
+                # add a new head
+                node.prev = self.head.prev
+                node.next = self.head
+                self.head = node
             head = head.next
+        
+        if (head.data == value and not changed):
+            # If its the last node, add as it is
+            node.prev = self.head.prev
+            self.head.prev = node
+            node.next = self.head
+            self.head = node
+
 
     def getLength(self):
-        count = 0
+        count = 1
         head = self.head
-        while head != None:
+        while head.next != self.head:
             count += 1
             head = head.next
         return count
@@ -96,18 +110,22 @@ class LinkedList:
     # Print all the elements of the linked list
     def printAll(self):
         head = self.head
-        while head != None:
+        while head.next != self.head:
             print(head.data)
             head = head.next
+        #print last element separately
+        print(head.data)
     
     # Print all the elements in the linked list in reverse
     def printReverse(self):
         # Iterate to the last element
         current = self.head
-        while current.next != None:
+        while current.next != self.head:
             current = current.next
         
         # Iterate back to the head element
-        while current != None:
+        while current != self.head:
             print(current.data)
             current = current.prev
+        print(current.data)
+
